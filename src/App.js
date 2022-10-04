657fd8e39a41460129b52c2d7eedbd651202c259
Jack Whale <jemwhale@gmail.com>
2022-10-04 17:08:58 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Container, FormControl } from 'react-bootstrap';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
 
+  const [pokemonList, setPokemonList] = useState([]);
+
+  // function getPokemon(){
+  //   try{
+  //     const res = fetch(pokeApi);
+  //     const data = res.json();
+  //     setPokemonList(data.results);
+  //   }catch{
+  //     console.log('Error fetching pokemon');
+  //   }
+  // }
+
+  useEffect (()=>{
+    fetch(pokeApi).then((response)=>response.json()).then((data)=>{setPokemonList(data.results)}).catch((error)=>{console.error(error)})
+    // getPokemon();
+  },[]);
+
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <Container>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="search">Search</InputGroup.Text>
+          <FormControl placeholder="Pokemon" aria-label="Pokemon" aria-describedby='basic-addon1'/>
+        </InputGroup>
+      {pokemonList.map((pokemon, index)=>{
+        return <PokemonCard key={pokemon.name} name={pokemon.name} url={pokemon.url}/>
+      })}
+      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState  } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function PokemonCard({ url, name }) {
 
+  const pokeApiPokemon = {url};
+
+  const [pokemon, setPokemon] = useState([]);
+
+  // function getAPokemon(){
+  //   try{
+  //     const res = fetch(url);
+  //     const data = res.json();
+  //     // console.log(data)
+  //     setPokemon(data);
+  //   }catch{
+  //     console.log('Error fetching a pokemon');
+  //   }
+  // }
+  
+  useEffect(()=>{
+    fetch(url).then((response)=>response.json()).then((data)=>{setPokemon(data)}).catch((error)=>{console.error(error)})
+    // getAPokemon();
+  },[])
+
   return (
-    <div>
-        pokemon card
-    </div>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={pokemon.sprites.front_default}/>
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text as="div">
+          <ul>
+            {pokemon.abilities.map((ability)=>{
+              return <li key={ability.ability.name}>{ability.ability.name}</li>
+            })}
+          </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
